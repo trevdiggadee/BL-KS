0864ed82be895c55f218de34801d1f164289bd2b
trevdiggadee <trevdiggadee@gmail.com>
2026-09-21 04:06:34 -0700

    Add files via upload

--- game.js
+++ game.js
@@ -263,7 +263,6 @@ const Game = (() => {
     const newLevel = Scoring.levelForLines(lines);
     const leveledUp = newLevel > level;
     level = newLevel;
-    if (leveledUp) Audio_.setLevel(level);
 
     saveData.stats.totalLines += fullRows.length;
     if (result.isTetris) saveData.stats.totalTetrises += 1;
@@ -452,7 +451,7 @@ const Game = (() => {
     } else if (state === 'paused') {
       state = 'playing';
       UI.setOverlay('overlay-pause', false);
-      if (saveData.settings.musicOn) Audio_.startMusic(level);
+      if (saveData.settings.musicOn) Audio_.startMusic();
       sessionStartTs = performance.now();
       lastTs = performance.now();
     }
@@ -633,7 +632,7 @@ const Game = (() => {
     state = 'playing';
     sessionStartTs = performance.now();
     lastTs = 0;
-    if (saveData.settings.musicOn) Audio_.startMusic(level);
+    if (saveData.settings.musicOn) Audio_.startMusic();
     rafId = requestAnimationFrame(loop);
   }
 
